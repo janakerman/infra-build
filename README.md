40cd20e78b92ca1e002fe021b54452fc1e33b0b9
Jan Akerman <j.f.akerman@gmail.com>
2021-11-07 22:09:08 +0000

    Create EKS cluster

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # infra-build
+
+Based on Terraform [guide](https://learn.hashicorp.com/tutorials/terraform/eks).
+
+## Connect
+
+Set cluster config:
+
+```shell
+aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
+```
--- tf/main.tf
+++ tf/main.tf
@@ -1,70 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
 provider "aws" {
   region = "eu-west-1"
   default_tags {
     tags = {
-      repo = "infra-build"
+      Repo = "infra-build"
+      Environment = "dev"
     }
   }
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.0.0/24"
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  route_table_id = aws_route_table.public.id
-  subnet_id = aws_subnet.public.id
-}
-
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
-}
-
-// Egress Internet access from all subnets by default
-resource "aws_default_route_table" "private" {
-  default_route_table_id = aws_vpc.vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.natgw.id
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_nat_gateway" "natgw" {
-  allocation_id = aws_eip.natgw.allocation_id
-  subnet_id = aws_subnet.public.id
-}
-
-resource "aws_eip" "natgw" {
-  vpc = true
-  depends_on = [aws_internet_gateway.igw]
+variable "region" {
+  default     = "eu-west-1"
+  description = "AWS region"
 }
